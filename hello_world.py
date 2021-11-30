0b651d0c8494eeef2bd119f0991a7969b719a9cd
KeithLion <nortonkiara6@gmail.com>
2021-11-29 20:11:32 -0800

    added favorite food strings

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
 print('Hello world')
+
+name = 'Noelle'
+print('Hello', name, '!')
+print('Hello', name + '!')
+
+name= 42
+print('Hello', name, '!')
+print('Hello', str(name)+'!')
+
+fave_food1 = 'sushi'
+fav_food2 = 'pizza'
+print('I love to eat {} and {}'.format(fave_food1,fav_food2))
+print(f'I love to eat {fave_food1} and {fav_food2}.')
